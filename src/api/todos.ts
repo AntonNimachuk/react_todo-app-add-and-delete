cf57ddb519a6a4952b37a07fe8d07e74721ba36e
Anton <nimachuka@gmail.com>
2026-05-24 00:31:04 +0200

    2nd commit to share

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -7,4 +7,12 @@ export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
+export const addTodo = (todoData: Omit<Todo, 'id'>): Promise<Todo> => {
+  return client.post<Todo>('/todos', todoData);
+};
+
+export const deleteTodo = (id: number) => {
+  return client.delete(`/todos/${id}`);
+};
+
 // Add more methods here
